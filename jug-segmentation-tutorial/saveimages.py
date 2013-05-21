7a0f76748e5a76d6a8b0be375d8dbd476ab30c32
Luis Pedro Coelho <luis@luispedro.org>
2013-05-21 12:05:30 +0200

    ENH Generate and save images

--- jug-segmentation-tutorial/saveimages.py
+++ jug-segmentation-tutorial/saveimages.py
@@ -5,7 +5,7 @@ import numpy as np
 
 im = mh.imread('images/dna-21.jpg')
 mh.imsave('image_stretched.jpeg', mh.stretch(im.astype(float)**.01))
-m1 = method1.f('images/dna-21.jpg')
+m1 = method1.f('images/dna-21.jpg', 2)
 m1 = m1.astype(np.uint8)
 color = ((cm.rainbow(m1.astype(float)/m1.max())[:,:,:3]).reshape(m1.shape+(3,)))
 color[m1 == 0] = (0,0,0)
--- python-meetup/build.sh
+++ python-meetup/build.sh
@@ -3,7 +3,7 @@ function build() {
     input=$1
     mkdir -p .$input.tex_files
     cd .$input.tex_files
-    TEXINPUTS=.:..:../../embl-template/:../images/:../figures/: xelatex $input
+    TEXINPUTS=.:..:../../embl-template/:../../images/:../figures/: xelatex $input
     cp $input.pdf ..
     cd ..
 }
