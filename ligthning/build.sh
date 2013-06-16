0ea8d6c3896f9ced4c26a9385b70ef5fb9e6c4e3
Luis Pedro Coelho <luis@luispedro.org>
2013-06-16 15:44:14 +0200

    STRCT Start a 5-minute presentation

---
+++ ligthning/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+function build() {
+    input=$1
+    mkdir -p .$input.tex_files
+    cd .$input.tex_files
+    TEXINPUTS=.:..:../../embl-template/:../../images/:../figures/: xelatex $input
+    cp $input.pdf ..
+    cd ..
+}
+build presentation
+
